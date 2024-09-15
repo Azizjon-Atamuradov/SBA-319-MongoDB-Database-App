aba0c3d0f72f5780fe8b9f9267ad04931d03c6ee
Aziz <azizatamuradov1994@gmail.com>
2024-09-15 13:10:24 -0400

    post

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const express = require ('express')
-const app = express ()
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
 
+const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send("Hello from Node API")
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
+
+app.post('/api/products', async (req, res) => {
+
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    
+});
+
+
+
+
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://azizatamuradov1994:a2225506@mongopractice.69ry0.mongodb.net/"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
