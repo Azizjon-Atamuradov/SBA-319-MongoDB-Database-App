b823eb8c80147c5917f0743ce79cfd2db5eb14de
Aziz <azizatamuradov1994@gmail.com>
2024-09-15 20:21:39 -0400

    product

--- index.js
+++ index.js
@@ -1,60 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js')
+const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js")
 
 const app = express();
 
+/// middleware
 app.use(express.json());
+
+
+/// routes
+app.use("/api/products", productRoute);
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products =  await Product.find({});
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-////////// find product by id
-
-app.get('/api/product/:id', async (req, res) => {
-
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    
-    } catch (error) {
-        res.status(500).json({message:error.message})
-    }
-
-})
-
-
-
-app.post('/api/products', async (req, res) => {
-
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-    
-});
-/// update product
-
-
-
-
-
-
+ 
+ 
 mongoose
-  .connect(
+.connect(
     "mongodb+srv://azizatamuradov1994:a2225506@mongopractice.69ry0.mongodb.net/"
   )
   .then(() => {
